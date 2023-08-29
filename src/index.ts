645e1888aa4a67698e9a203fa007f3e07ff8608f
Mike Robinson <mrobinson@wrapbook.com>
2023-08-29 14:16:25 -0400

    clean it up

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as core from '@actions/core'
 import * as artifact from '@actions/artifact'
 
 export async function run(): Promise<void> {
+  const runId = core.getInput('run-id') // but don't use it for anything...
   const downloadResponse = await artifact.create().downloadAllArtifacts()
   for (const resp of downloadResponse) {
     console.log(fs.readFileSync(resp.downloadPath))
