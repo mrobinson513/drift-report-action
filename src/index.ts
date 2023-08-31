469dd24dcccf1f0ab9dc81728b630d1adca352cb
Mike Robinson <mrobinson@wrapbook.com>
2023-08-31 15:21:18 -0400

    try template render

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,19 @@ import * as fs from 'fs'
 import * as core from '@actions/core'
 import * as artifact from '@actions/artifact'
 
+let ejs = require('ejs')
+
 export async function run(): Promise<void> {
   const runId = core.getInput('run-id') // but don't use it for anything...
   const downloadResponse = await artifact.create().downloadAllArtifacts()
   for (const resp of downloadResponse) {
       const dir = resp.artifactName
+      const tgPath = dir.replaceAll('_','/');
+      console.log(
+        ejs.render(
+          'Terragrunt Path: <%= path %>', {path: tgPath}
+        )
+      )
       fs.readdir(resp.downloadPath, (err,files) => {
         if (err)
           console.log(err)
